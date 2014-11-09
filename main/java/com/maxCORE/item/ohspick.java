e94dcf02c61227291810cbfbce5bf614646ea106
TekCastElemental <Max560gaming@gmail.com>
2014-11-09 17:53:34 -0600

    MaxCORE 2.0 release

---
+++ main/java/com/maxCORE/item/ohspick.java
@@ -0,0 +1,12 @@
+package com.maxCORE.item;
+
+import net.minecraft.item.ItemPickaxe;
+
+public class ohspick extends ItemPickaxe{
+
+	public ohspick(int i, ToolMaterial p_i45347_1_) {
+		super(p_i45347_1_);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ main/java/com/maxCORE/item/ohssword.java
@@ -0,0 +1,12 @@
+package com.maxCORE.item;
+
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.item.ItemSword;
+
+public class ohssword extends ItemSword{
+
+	public ohssword(int i, ToolMaterial p_i45356_1_) {
+		super(p_i45356_1_);
+		// TODO Auto-generated constructor stub
+	}
+}
