ae18fb616581483c73fadceba4afab1809c98634
TekCastElemental <Max560gaming@gmail.com>
2014-11-09 17:55:32 -0600

    Revert "Revert "MaxCORE 2.0 release""
    This reverts commit 1d2e335cd0bee37cd6f961a8d6d6b812bfd5897e.

---
+++ main/java/com/maxCORE/item/ohsspade.java
@@ -0,0 +1,14 @@
+package com.maxCORE.item;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.item.ItemSpade;
+
+public class ohsspade extends ItemSpade{
+
+	public ohsspade(int i, ToolMaterial p_i45347_1_) {
+		super(p_i45347_1_);
+		// TODO Auto-generated constructor stub
+	}
+
+}
